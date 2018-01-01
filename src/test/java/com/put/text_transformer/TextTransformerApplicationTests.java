ce165bcea22774c6dbaac308200b35b016f76e24
Jakub Tomczak <jakub.tomczak@live.com>
2018-01-01 21:58:27 +0100

    Initial commit.

---
+++ src/test/java/com/put/text_transformer/TextTransformerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.put.text_transformer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TextTransformerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
