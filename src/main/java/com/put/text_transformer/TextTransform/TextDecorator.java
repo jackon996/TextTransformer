ddb8871eb5acc3357c0efb7aadeea9ffe17945ce
Jakub Tomczak <jakub.tomczak@live.com>
2018-01-03 15:49:20 +0100

    Add comments.

--- src/main/java/com/put/text_transformer/TextTransform/TextDecorator.java
+++ src/main/java/com/put/text_transformer/TextTransform/TextDecorator.java
@@ -1,17 +1,20 @@
 package com.put.text_transformer.TextTransform;
 
 
-/**
- * Created by Master Faster on 03.01.2018.
- */
 public class TextDecorator implements IText {
 
     protected IText textInput;
 
+    /**
+     * @param textInput to be transformed.
+     */
     public TextDecorator(IText textInput){
         this.textInput = textInput;
     }
 
+    /**
+     * @return Transformation.
+     */
     @Override
     public String transform() {
         return this.textInput.transform();
--- src/main/java/com/put/text_transformer/TextTransform/UpperText.java
+++ src/main/java/com/put/text_transformer/TextTransform/UpperText.java
@@ -5,10 +5,16 @@ package com.put.text_transformer.TextTransform;
  */
 public class UpperText extends TextDecorator {
 
+    /**
+     * @param textInput injected IText interface.
+     */
     public UpperText(IText textInput){
         super(textInput);
     }
 
+    /**
+     * @return Transformation that uppers all characters in the text.
+     */
     @Override
     public String transform(){
         String text = textInput.transform();
