6f6ef715ed29cfb0088228d0c9ad6e08dfe55e9c
masterfaster <jerzyroman96@gmail.com>
2018-01-03 14:03:56 +0100

    Lower decorator added

--- src/main/java/com/put/text_transformer/TextTransform/TransformController.java
+++ src/main/java/com/put/text_transformer/TextTransform/TransformController.java
@@ -14,9 +14,25 @@ import org.springframework.web.bind.annotation.RestController;
 public class TransformController {
 
     @RequestMapping(value ="", method = RequestMethod.GET)
-    public String getText(@RequestParam("text") String text){
+    public String getText(@RequestParam("text") String text,
+                            @RequestParam("transformTable") String[] transformTable){
         TextInput textInput = new TextInput(text);
-        TextDecorator textDecorator = new UpperText(new InverseText(textInput));
+        TextDecorator textDecorator = new TextDecorator(textInput);// = new UpperText(new InverseText(textInput));
+        for(String transformation : transformTable){
+            if(transformation.equals("inverse")){
+                System.out.println("Inversing text...");
+                textDecorator = new InverseText(textDecorator);
+            }
+            if(transformation.equals("upper")){
+                System.out.println("Uppering text...");
+                textDecorator = new UpperText(textDecorator);
+            }
+            if(transformation.equals("lower")){
+                System.out.println("Lowering text...");
+                textDecorator = new LowerText(textDecorator);
+            }
+        }
+
         return textDecorator.transform();
     }
 
--- src/test/java/com/put/text_transformer/TextTransformerApplicationTests.java
+++ src/test/java/com/put/text_transformer/TextTransformerApplicationTests.java
@@ -5,6 +5,8 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.validation.constraints.AssertTrue;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TextTransformerApplicationTests {
