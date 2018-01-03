3516944aefe303d656119319798e24e81973f52b
masterfaster <jerzyroman96@gmail.com>
2018-01-03 16:22:02 +0100

    documentation update

--- src/main/java/com/put/text_transformer/TextTransform/TextInput.java
+++ src/main/java/com/put/text_transformer/TextTransform/TextInput.java
@@ -4,8 +4,13 @@ import com.put.text_transformer.TextTransform.IText;
 
 /**
  * Created by Master Faster on 03.01.2018.
+ * Obiekt, który będzie dekorowany przez dekoratory
  */
 public class TextInput implements IText {
+
+    /**
+     *tekst przetwarzany przez dekoratory
+     */
     private String text;
 
     public TextInput(String text){
