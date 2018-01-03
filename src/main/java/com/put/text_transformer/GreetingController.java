c501ad71a29729fc80e118fbc20873aed16ef72f
Jakub Tomczak <jakub.tomczak@live.com>
2018-01-03 12:38:24 +0100

    Change text.

--- src/main/java/com/put/text_transformer/GreetingController.java
+++ src/main/java/com/put/text_transformer/GreetingController.java
@@ -8,11 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-    private static final String template = "Hello, %s!";
+    private static final String template = "Witaj, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+    public Greeting greeting(@RequestParam(value="name", defaultValue="Janusz") String name) {
         return new Greeting(counter.incrementAndGet(),
                 String.format(template, name));
     }
