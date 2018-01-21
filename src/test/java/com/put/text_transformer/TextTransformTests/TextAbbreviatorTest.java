09d24fb9a69e6ab3e53783fcdef68864320acd05
kubzdel <kubzdel@gmail.com>
2018-01-21 20:12:54 +0100

    5 testing classes, 10 methods.

--- src/test/java/com/put/text_transformer/TextTransformTests/TextAbbreviatorTest.java
+++ src/test/java/com/put/text_transformer/TextTransformTests/TextAbbreviatorTest.java
@@ -12,16 +12,22 @@ import static org.junit.Assert.*;
 public class TextAbbreviatorTest {
 
     @Test
-    public void abbreviator() throws Exception {
-        assertEquals("elo",new AbbreviatorText(new TextInput("elo")).transform());
+    public void naPrzyklad() throws Exception {
         assertEquals("na przykład","np.",new AbbreviatorText(new TextInput("na przykład")).transform());
+        assertEquals("Jurek lubi nP. kolegów",new AbbreviatorText(new TextInput("Jurek lubi na Przykład kolegów")).transform());
         assertEquals("NP.",new AbbreviatorText(new TextInput("NA PRZykład")).transform());
+    }
+
+    @Test
+    public void miedzyInnymi() throws Exception {
+        assertEquals("na przykład","np.",new AbbreviatorText(new TextInput("na przykład")).transform());
         assertEquals("Jurek lubi nP. kolegów",new AbbreviatorText(new TextInput("Jurek lubi na Przykład kolegów")).transform());
         assertEquals("M.in. Kuba nie zrobił nic przy projekcie",new AbbreviatorText(new TextInput("Między innymi Kuba nie zrobił nic przy projekcie")).transform());
         assertEquals("M.iN. Kuba nie zrobił nic przy projekcie",new AbbreviatorText(new TextInput("Między iNnymi Kuba nie zrobił nic przy projekcie")).transform());
+    }
+    @Test
+    public void iTymPodobne() throws Exception {
         assertEquals("AAa itp. Bba",new AbbreviatorText(new TextInput("AAa i tym podobne Bba")).transform());
         assertEquals("Itp.",new AbbreviatorText(new TextInput("I tym podobne")).transform());
     }
-
-
 }
